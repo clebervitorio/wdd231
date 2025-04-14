691876617bd11971b9ff65fa2c7a0f7d39179ccf
clebervitorio <clebervitorio@gmail.com>
2025-04-14 14:24:54 +0100

    W06 Project: Completion - update 05

--- project/scripts/datetime.js
+++
@@ -1,6 +0,0 @@
-
-// Dynamically set the current year
-document.getElementById('current-year').textContent = new Date().getFullYear();
-
-// Dynamically set the last modified date
-document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
